cbe801dc3f545354616d9a60a276f064134711f8
bobbeekethdy1025 <bobbeekethdy@gmail.com>
2022-10-25 15:50:15 -0500

    finished up routes

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,8 +1,6 @@
-
 const {Schema, Types} = require('mongoose');
 const moment = require('moment');
 
-// schema to create reaction virtual
 const reactionSchema = new Schema({
     reactionId: {type: Schema.Types.ObjectId, default: () => new Types.ObjectId()},
     reactionBody: {type: String, required: true, maxlength: 280},
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,19 @@
+
+const router = require('express').Router();
+
+const {
+    getThoughts, getOneThought, createThought, updateThought, deleteThought, createReaction, deleteReaction,
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId')
+    .get(getOneThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
